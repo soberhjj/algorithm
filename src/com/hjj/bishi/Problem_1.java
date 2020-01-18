fe1e36c7d17e7d514899138d03998c00377286d2
sober <sober@163.com>
2020-01-18 15:29:08 +0800

    commit

--- src/com/hjj/bishi/Problem_1.java
+++ src/com/hjj/bishi/Problem_1.java
@@ -5,14 +5,13 @@ package com.hjj.bishi;
  */
 
 /**
- * problem describe:
+ * problem describe:题目：二维数组查找
  * 在一个二维数组中（每个一维数组的长度相同），每一行都按照从左到右递增的顺序排序，
  * 每一列都按照从上到下递增的顺序排序。请完成函数Find，输入这样的一个二维数组和一个整数，
  * 判断数组中是否含有该整数。
  */
 public class Problem_1 {
     public static void main(String[] args) {
-//        int [][] test={{3,5,7,9},{4,6,8,10},{6,7,9,11},{10,12,14,18},{14,15,19,21}};
         int [][] test={{1,2,8,9},{2,4,9,12},{4,7,10,13},{6,8,11,15}};//初始化二维数组的三种方式 https://blog.csdn.net/Code_lr/article/details/78538937
         boolean result=new Problem_1().Find(16,test);
         System.out.println(result);
