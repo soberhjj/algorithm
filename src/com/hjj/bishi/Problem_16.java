f54fa76ff374ad9054a47a2d0d32b3d2bb6e9a78
sober <sober@163.com>
2020-01-23 15:52:24 +0800

    commit

---
+++ src/com/hjj/bishi/Problem_16.java
@@ -0,0 +1,22 @@
+package com.hjj.bishi;
+
+/**
+ * @author soberhjj  2020/1/23 - 11:53
+ */
+//反转链表
+//输入一个链表，反转链表后，输出新链表的表头。
+public class Problem_16 {
+    public ListNode ReverseList(ListNode head) {
+        if (head == null)
+            return null;
+        ListNode p = head.next;
+        head.next = null; //表头变表尾
+        while (p != null) {
+            ListNode next = p.next;
+            p.next=head;
+            head = p;
+            p = next;
+        }
+        return head;
+    }
+}
