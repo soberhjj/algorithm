3e9e5225af63c41467b351483a9d776e8d8e97e2
sober <sober@163.com>
2020-01-23 10:51:35 +0800

    commit

--- src/com/hjj/bishi/Problem_10.java
+++ src/com/hjj/bishi/Problem_10.java
@@ -7,10 +7,11 @@ package com.hjj.bishi;
 //一只青蛙一次可以跳上1级台阶，也可以跳上2级……它也可以跳上n级。
 //求该青蛙跳上一个n级的台阶总共有多少种跳法。
 //本题与Problem_9跳台阶 参考：https://blog.csdn.net/Darlin_F/article/details/81278995
+//本题也是斐波那契数列的应用
 public class Problem_10 {
     public int JumpFloorII(int target) {
         if (target < 0)
-            return -1;
+            return 0;
         if (target == 0 || target == 1)
             return 1;
         return 2*JumpFloorII(target-1);
---
+++ src/com/hjj/bishi/Problem_11.java
@@ -0,0 +1,20 @@
+package com.hjj.bishi;
+
+/**
+ * @author soberhjj  2020/1/23 - 9:30
+ */
+//题目：矩形覆盖
+//我们可以用2*1的小矩形横着或者竖着去覆盖更大的矩形。
+//请问用n个2*1的小矩形无重叠地覆盖一个2*n的大矩形，总共有多少种方法？
+//本题也是斐波那契数列的应用。
+public class Problem_11 {
+    public int RectCover(int target) {
+        if (target<=0)
+            return 0;
+        if (target==1)
+            return 1;
+        if (target==2)
+            return 2;
+        return RectCover(target-1)+RectCover(target-2);
+    }
+}
