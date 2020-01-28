8e33358f822ed3924c01fc3a510600ae58f73ef7
sober <sober@163.com>
2020-01-28 11:39:41 +0800

    commit

---
+++ src/com/hjj/bishi/Problem_19.java
@@ -0,0 +1,20 @@
+package com.hjj.bishi;
+
+/**
+ * @author soberhjj  2020/1/28 - 11:29
+ */
+//题目：二叉树的镜像
+//操作给定的二叉树，将其变换为源二叉树的镜像。
+public class Problem_19 {
+    public void Mirror(TreeNode root) {
+        if (root == null)
+            return;
+        if (root.left != null || root.right != null) {
+            TreeNode temp = root.left;
+            root.left = root.right;
+            root.right = temp;
+        }
+        Mirror(root.left);
+        Mirror(root.right);
+    }
+}
